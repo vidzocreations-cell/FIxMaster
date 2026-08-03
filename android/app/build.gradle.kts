071c449195700f899d980d77af78a154ffc8e143
HansaMadushan <madushan.hansa@gmail.com>
2026-08-03 15:14:13 +0530

    Update ultra-professional 3D Metallic Wrench & Gear app icon for Android v4.0 release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.fixmasterpos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
     }
 
     buildTypes {
